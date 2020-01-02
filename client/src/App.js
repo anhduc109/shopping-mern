05c179a935edca5003b1fe90414c6589efcbab29
Duc Pham <ducpham1098@gmail.com>
2020-01-02 17:12:31 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import AppNavBar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+import ItemModal from "./components/ItemModal";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Container } from "reactstrap";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <AppNavBar />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://duc:123@mern-shopping-list-f7ohs.mongodb.net/test?retryWrites=true&w=majority"
+};
